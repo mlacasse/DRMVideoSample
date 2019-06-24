b9e4d266bd4e1afe731a4bc96adaeccc76df2948
Marc Lacasse <marc.lacasse@mac.com>
2019-06-24 16:50:39 -0700

    Added clear stream video + started FairPlay DRM setup

--- app/YiReactApp.js
+++ app/YiReactApp.js
@@ -1,14 +1,118 @@
+/**
+ * Sample app to demonstrate using a custom video module that uses a custom player integration.
+ * The implementation is as follows:
+ *  (1) Create a Custom Player to bypass using the the native abstract player for iOS (App.cpp)
+ *  (2) Implement platform specific players for iOS (CustomVideoPlayer.cpp)
+ *  (3) Demonstrate how to communicate with Custom Player with functionality outside of CYIAbstractVideoPlayer
+ * @flow
+ */
+
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import { DeviceInfo, FairPlayDrmHandler, WidevineCustomRequestDrmHandler, Video } from '@youi/react-native-youi';
 
 class YiReactApp extends Component {
   constructor() {
     super();
+
+    this.streamInfo = getStreamInfo();
+    this.videoPlayer = null;
+  }
+
+  componentWillMount() {
+    if (this.streamInfo.drmScheme === 'fairplay') {
+      FairPlayDrmHandler.addEventListener('DRM_REQUEST_URL_AVAILABLE', this.onFairplayDRMRequestUrlAvailable);
+      FairPlayDrmHandler.addEventListener('SPC_MESSAGE_AVAILABLE', this.onFairplaySPCMessageAvailable);  
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.streamInfo.drmScheme === 'fairplay') {
+      FairPlayDrmHandler.removeEventListener('DRM_REQUEST_URL_AVAILABLE', this.onFairplayDRMRequestUrlAvailable);
+      FairPlayDrmHandler.removeEventListener('SPC_MESSAGE_AVAILABLE', this.onFairplaySPCMessageAvailable);
+    }
+  }
+
+  onFairplayDRMRequestUrlAvailable = (args) => {
+  }
+
+  onFairplaySPCMessageAvailable = (args) => {
+  }
+
+  onLoginsucceeded = (args) => {
+  }
+
+  onTokenSuccess = (args) => {
+  }
+
+  handleOnReady = () => {
+    if (this.videoPlayer) {
+      this.videoPlayer.play();
+    }
+  }
+
+  handleOnErrorOccurred = error => {
+    console.log("onErrorOccurred: ", error);
   }
 
   render() {
-    <View></View>
+    return(
+      <View style={styles.containerStyle}>
+        <Video 
+          style={styles.videoStyle}
+          ref={ ref => this.videoPlayer = ref }
+          source={this.streamInfo}
+          onErrorOccurred={this.handleOnErrorOccurred}
+          onDurationChanged={() => {}}
+          onCurrentTimeUpdated={() =>{}}
+          onReady={this.handleOnReady}
+        />
+      </View>
+    );
+  }
+};
+
+const getStreamInfo = () => {
+  const clearStreamInfo = {
+    uri: 'https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8',
+    type: 'HLS'
   };
+
+  if (DeviceInfo.getDeviceModel() === 'Simulator') {
+    return clearStreamInfo;
+  }
+
+  switch(DeviceInfo.getDeviceManufacturer()) {
+    case 'Apple':
+      return {
+        uri: 'https://dtv-latam-abc.akamaized.net/hls/live/2003011-b/dtv/dtv-latam-boomerang/master.m3u8',
+        type: 'HLS',
+        drmScheme: 'fairplay',
+        drmInfo: null
+      };
+    case 'Android':
+      return {
+          uri: 'https://dtv-latam-abc.akamaized.net/hls/live/2003011-b/dtv/dtv-latam-boomerang/master.m3u8',
+          type: 'HLS',
+          drmScheme: 'widevine',
+          drmInfo: null
+        };
+    default:
+      return clearStreamInfo;
+  }
+}
+
+const styles = {
+  containerStyle: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  videoStyle: {
+    position: 'absolute',
+    width: 1920,
+    height: 1080
+  }
 };
 
 export default YiReactApp;
--- youi/src/App.cpp
+++ youi/src/App.cpp
@@ -4,6 +4,13 @@
 #include <cxxreact/JSBigString.h>
 #include <glog/logging.h>
 
+#include <logging/YiLogger.h>
+#include <logging/YiLoggerHelper.h>
+#include <framework/YiFramework.h>
+
+#include <youireact/modules/drm/FairPlayDrmHandlerModule.h>
+#include <network/YiHTTPService.h>
+
 #if defined(YI_LOCAL_JS_APP)
 #    if defined(YI_INLINE_JS_APP)
 #        include "youireact/JsBundleLoaderInlineString.h"
@@ -25,6 +32,33 @@ using namespace yi::react;
 
 bool App::UserInit()
 {
+    // Setup Logging Preferences
+    std::shared_ptr<CYIPreferences> pPreferences(new CYIPreferences());
+    
+    // App wide Log preferences
+    pPreferences->Set("TAG_GENERAL", "DEBUG");
+    
+    // Error messages
+    pPreferences->Set("TAG_CYIAssetDownloadHelper", "ERROR");
+    pPreferences->Set("TAG_CYIHTTPService", "ERROR");
+    pPreferences->Set("TAG_CYIHTTPServiceStats", "ERROR");
+    pPreferences->Set("TAG_CYIImageView", "ERROR");
+    pPreferences->Set("TAG_CYILRUCache", "ERROR");
+    pPreferences->Set("TAG_CYIPersistentCache", "ERROR");
+    pPreferences->Set("TAG_CYITransferHandle", "ERROR");
+    pPreferences->Set("TAG_EventDispatcherModule", "ERROR");
+    pPreferences->Set("TAG_CYIExoPlayer", "ERROR");
+    pPreferences->Set("TAG_ShadowTree", "ERROR");
+    pPreferences->Set("TAG_UIManagerModule", "ERROR");
+    
+    // Debug messages
+    pPreferences->Set("TAG_Transfer", "DEBUG");
+    
+    // Info messages
+    pPreferences->Set("TAG_JavaScript", "INFO");
+    
+    CYILogger::SetPreferences(pPreferences);
+
     // Start the web driver for allowing the use of Appium.
     CYIWebDriver *pWebDriver = CYIWebDriverLocator::GetWebDriver();
     if (pWebDriver)
@@ -53,7 +87,13 @@ bool App::UserInit()
 #endif
 
     PlatformApp::SetJsBundleLoader(std::move(pBundleLoader));
-    return PlatformApp::UserInit();
+    bool result = PlatformApp::UserInit();
+
+    GetBridge().AddModule<FairPlayDrmHandlerModule>();
+    
+    CYIHTTPService::GetInstance()->ClearCache();
+
+    return result;
 }
 
 bool App::UserStart()
