f7281532d3b5987bbbe88d14d7023ea77627e96c
Marc Lacasse <marc.lacasse@mac.com>
2019-06-24 16:50:39 -0700

    Added clear stream video + started FairPlay DRM setup

--- index.youi.js
+++ index.youi.js
@@ -1,4 +1,4 @@
-import { AppRegistry } from '@youi/react-native-youi';
+import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
 import YiReactApp from './app/YiReactApp';
 
