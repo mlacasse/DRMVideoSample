9811af392c39768fc85b5f3d05dfeaae2ac0e1cf
Marc Lacasse <marc.lacasse@mac.com>
2019-09-26 14:28:17 -0400

    Added GlobalThis shim ... shouldn't do this

--- app/AppComponent.js
+++ app/AppComponent.js
@@ -13,7 +13,21 @@ import { CLEARStream } from './store/stream';
 
 const { Dimensions, OrientationLock } = NativeModules;
 
-self.Image = () => {};
+// A naive globalThis shim. Don’t use this!
+const getGlobalThis = () => {
+	if (typeof globalThis !== 'undefined') return globalThis;
+	if (typeof self !== 'undefined') return self;
+	if (typeof window !== 'undefined') return window;
+	if (typeof global !== 'undefined') return global;
+	if (typeof this !== 'undefined') return this;
+	throw new Error('Unable to locate global `this`');
+};
+// Note: `var` is used instead of `const` to ensure `globalThis`
+// becomes a global variable (as opposed to a variable in the
+// top-level lexical scope) when running in the global scope.
+var globalThis = getGlobalThis();
+
+globalThis.Image = () => {};
 
 Object.defineProperty(Image.prototype, 'src', {
   get() { return src; },
