677f42d10d3b07b9e290fdc16d3a11956978c5ee
Marc Lacasse <marc.lacasse@mac.com>
2020-02-13 14:24:19 -0500

    Fixed method name in native module stub

--- youi/src/AirplayModule.cpp
+++ youi/src/AirplayModule.cpp
@@ -14,7 +14,7 @@ AirplayModule::AirplayModule()
 AirplayModule::~AirplayModule()
 {}
 
-YI_RN_DEFINE_EXPORT_METHOD(AirplayModule, setExternalPlayback)(uint64_t tag, bool bEnable)
+YI_RN_DEFINE_EXPORT_METHOD(AirplayModule, setExternalAutoPlayback)(uint64_t tag, bool bEnable)
 {
     YI_UNUSED(tag);
     YI_UNUSED(bEnable);
